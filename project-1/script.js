553db91cd510172f5734565b36a4204d9d23084e
gokulsrinath <mgokul06@gmail.com>
2023-02-16 20:47:14 +0530

    model-octopus-view: seperating view and model

--- project-1/script.js
+++ project-1/script.js
@@ -4,9 +4,12 @@
 // let cimg = document.querySelectorAll(".img");
 let catNames = ["timmy", "jimmy"];
 let catLinks = ["./cat.jpeg", "./cat2.jpg"];
-let nCats = 5;
+let nCats = 2;
 let body = document.querySelector("body");
 body.innerHTML = "";
+
+
+let catEles =[]
 
 function createElement(type, classNames, attr = []) {
   let ele = document.createElement(type);
@@ -21,56 +24,83 @@ function createElement(type, classNames, attr = []) {
   return ele;
 }
 
+for (let i = 0; i < nCats; i++) {
+    let cat = createElement("div", "cat1 cat");
+
+    let header = createElement("h2", "name");
+    header.innerHTML = catNames[i%2];
+    cat.append(header);
+  
+    let imgDiv = createElement("div", "click");
+  
+    imgDiv.append(
+      createElement("img", "img", [
+        { key: "alt", val: "cat" },
+        { key: "height", val: "450px" },
+        { key: "width", val: "350px" },
+        { key: "src", val: catLinks[i%2] },
+      ])
+    );
+  
+    let controlDiv = createElement("div", "control");
+  
+    let contentControl = createElement("div");
+  
+    contentControl.innerHTML = "Number of Clicks : ";
+  
+    let spanEle = createElement("span", "counter");
+    spanEle.innerHTML = "0";
+  
+    contentControl.append(spanEle);
+    controlDiv.append(contentControl);
+  
+    imgDiv.addEventListener("click", () => {
+      spanEle.innerHTML = parseInt(spanEle.innerHTML) + 1;
+    });
+  
+    cat.append(imgDiv);
+    cat.append(controlDiv);
+    catEles.push(cat);
+  }
+  
 
 
 let main = createElement("div", "main");
 
-for (let i = 0; i < nCats; i++) {
-  let cat = createElement("div", "cat1");
-  let header = createElement("h2", "name");
-  header.innerHTML = catNames[i%2];
-  cat.append(header);
+let section1 = createElement("div","section1");
+let section2 = createElement("div","section2");
 
-  let imgDiv = createElement("div", "click");
+section2.innerHTML = `<h2 class="default">Picture here</h2>`
+let ol = createElement("ol",undefined,[{key:"type",val:"A"}]);
 
-  imgDiv.append(
-    createElement("img", "img", [
-      { key: "alt", val: "cat" },
-      { key: "height", val: "450px" },
-      { key: "width", val: "350px" },
-      { key: "src", val: catLinks[i%2] },
-    ])
-  );
+for(let i=0;i<nCats;i++) {
+    let li = createElement("li","elems")
+    li.innerHTML = catNames[i];
+    li.addEventListener('click',() => {
+        console.log(section2.lastChild)
+        if(section2.hasChildNodes()) {
+            section2.replaceChild(catEles[i], section2.lastChild)
+        
+        }
 
-  let controlDiv = createElement("div", "control");
-
-  let contentControl = createElement("div");
-
-  contentControl.innerHTML = "Number of Clicks : ";
-
-  let spanEle = createElement("span", "counter");
-  spanEle.innerHTML = "0";
-
-  contentControl.append(spanEle);
-  controlDiv.append(contentControl);
-
-  imgDiv.addEventListener("click", () => {
-    spanEle.innerHTML = parseInt(spanEle.innerHTML) + 1;
-  });
-
-  cat.append(imgDiv);
-  cat.append(controlDiv);
-  main.append(cat);
-  body.append(main);
+        section2.appendChild(catEles[i])
+    })
+    ol.append(li);
 }
 
-// console.log(counter,click)
+section1.append(ol);
 
-// for(let i=0;i<2;i++) {
 
-//     cimg[i].setAttribute('src',catLinks[i])
-//     cname[i].innerHTML = catNames[i]
-//     click[i].addEventListener('click',()=>{
-//         counter[i].innerHTML = parseInt(counter[i].innerHTML) + 1
-//     })
-// }
+
+
+  main.append(section1);
+  main.append(section2);
+  body.append(main);
+
+
+
+
+
+
+
+
