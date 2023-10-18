0545cbe28c57fd785b474c6ae100addd438f78d8
Gokul srinath <mgokul06@gmail.com>
2023-10-18 12:17:35 +0530

    revised abfp

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import abstract_factory_pattern.factories.ModernFactory;
 import abstract_factory_pattern.factories.ProductFactory;
 import abstract_factory_pattern.products.chair.ChairProduct;
 import abstract_factory_pattern.products.sofa.SofaProduct;
-import factory_pattern.creator.*;
-import factory_pattern.products.Product;
-import factory_pattern.products.clothes.Clothing;
-import factory_pattern.products.cosmetics.Cosmetics;
-import factory_pattern.products.electronics.Electronics;
+//import factory_pattern.creator.*;
+//import factory_pattern.products.Product;
+//import factory_pattern.products.clothes.Clothing;
+//import factory_pattern.products.cosmetics.Cosmetics;
+//import factory_pattern.products.electronics.Electronics;
 
 public class Main {
 
